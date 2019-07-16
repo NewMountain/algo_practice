505928f8e1a5ff5023425296f0faae7eca879a88
Christopher Nyberg <christopher.nyberg@gmail.com>
2019-07-16 16:33:04 -0700

    Added the specs

---
+++ chapter_3/3_5.py
@@ -0,0 +1,7 @@
+"""Chapter 3: Stacks and Queues. Question 3.5"""
+
+# Question: Write a program to sort a stack such that the smallest items
+# are on top. You can use another temporary stack, but not copy data
+# to any other data structure.
+
+# The Stack supports push, pop, peek and isEmpty
---
+++ chapter_3/3_6.py
@@ -0,0 +1,4 @@
+"""Chapter 3: Stacks and Queues. Question 3.6"""
+
+# An animal shelter that holds cats and dogs and operates strictly on FIFO basis.
+# Your animal shelter must support enqueue(), dequeueAny(), dequeueDog(), dequeueCat()
