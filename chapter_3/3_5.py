15d2cef2490a83ade18b988ceccdb96393fd65c4
Christopher Nyberg <christopher.nyberg@gmail.com>
2019-07-17 19:26:35 -0700

    Class defined

--- chapter_3/3_5.py
+++ chapter_3/3_5.py
@@ -5,3 +5,46 @@
 # to any other data structure.
 
 # The Stack supports push, pop, peek and isEmpty
+
+from collections import deque
+
+# Let's define a stack object
+class Stack:
+    """LIFO stack."""
+
+    def __init__(self):
+        """Init."""
+        self.contents = deque()
+        self.depth = 0
+
+    def __repr__(self):
+        """Print function."""
+        return f"contents:\n{self.contents}"
+
+    def push(self, value):
+        """Push an element onto the stack."""
+        if value is None:
+            raise Exception("Null value not allowed")
+
+        self.contents.append(value)
+
+    def pop(self):
+        """Pop the last element off the stack."""
+        if self.is_empty():
+            return None
+
+        return self.contents.pop()
+
+    def peek(self):
+        """Look at the next element without altering stack."""
+        if self.is_empty():
+            return None
+
+        # Otherwise, return the peek
+        value = self.contents.pop()
+        self.contents.append(value)
+        return value
+
+    def is_empty(self):
+        """Return true is empty."""
+        return self.depth == 0
