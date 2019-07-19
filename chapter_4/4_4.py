acd3d3897b4da081c40ff88f572f15f9d45c8fe2
Christopher Nyberg <christopher.nyberg@gmail.com>
2019-07-18 22:18:43 -0700

    Placeholder questions

---
+++ chapter_4/4_4.py
@@ -0,0 +1,5 @@
+"""Chapter 4: Trees and Graphs. Question 4.4"""
+
+# Implement a function to check if a binary tree is balanced
+# Balanced is defined as the heights of the two subtrees never differ
+# by more than one
---
+++ chapter_4/4_5.py
@@ -0,0 +1,4 @@
+"""Chapter 4: Trees and Graphs. Question 4.4"""
+
+# Implement a function to test if a binary tree is
+# a binary search tree
